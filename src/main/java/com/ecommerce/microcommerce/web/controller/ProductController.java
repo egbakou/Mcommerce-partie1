1ef83e785ea81d889e6148fc771b859c33b2335e
egbakou <egbakoukodjolaurent@gmail.com>
2018-10-15 07:20:42 +0000

    Partie 1 - Affichage de la marge

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -16,7 +16,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Api( description="API pour es opérations CRUD sur les produits.")
@@ -26,6 +28,23 @@ public class ProductController {
 
     @Autowired
     private ProductDao productDao;
+    
+    
+    //Partie 1 - Affichage de la marge
+    
+    @GetMapping(value = "/AdminProduits")
+    public Map<Product, Integer> calculerMargeProduit() {
+        
+    	List<Product> listeProduits = productDao.findAll();
+        
+    	Map<Product, Integer> produitsAvecMarges = new HashMap<>();
+    	
+    	for(Product produit : listeProduits) {
+    		produitsAvecMarges.put(produit, produit.getPrix() - produit.getPrixAchat());
+    	}
+    	
+    	return produitsAvecMarges;
+    }
 
 
     //Récupérer la liste des produits
